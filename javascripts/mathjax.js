feb031f4930e6defc319fc5c7d4c8304969a95c6
Wayne <zhaoqiucheng21@gmail.com>
2026-03-14 17:14:31 +0800

    Deployed 13eec21 with MkDocs version: 1.6.0

---
+++ javascripts/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  /* 监听 MkDocs Material 页面的动态加载并重新渲染公式 */
+  document$.subscribe(() => {
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+  })
